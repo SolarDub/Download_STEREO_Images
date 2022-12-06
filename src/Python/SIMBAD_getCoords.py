e12bc1c320b8219f7eee4ba49113a32c02894010
SolarDub <81772405+SolarDub@users.noreply.github.com>
2022-12-05 21:05:16 -0500

    Update SIMBAD_getCoords.py

--- src/Python/SIMBAD_getCoords.py
+++ src/Python/SIMBAD_getCoords.py
@@ -4,16 +4,8 @@ import sys
 import astropy
 from astroquery.simbad import Simbad
 
-star = str(sys.argv[1])
+# Perform query on star (name passed by single argument)
+t = Simbad.query_object(str(sys.argv[1]))
 
-t = Simbad.query_object(star)
-
-for ra, dec in t.iterrows('RA', 'DEC'):
-    print("\nStar:\t{}\nRA:\t{}\nDEC:\t{}\n".format(star,ra,dec))
-    
-print(ra)
-print(dec)
-
-coords = [ra, dec]
-
-sys.exit(coords)
+# Output RA and DEC values from query output table
+sys.exit([t['RA'][0],t['DEC'][0]])
